faf3b2d97abe5d196f22908d241734b19cf14c3a
Hack Reactor Students <accounts@hackreactor.com>
2015-05-25 15:20:58 -0700

    Complete tests up to events

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,52 @@
 // YOUR CODE HERE:
+var app = {
+  server: 'https://api.parse.com/1/classes/chatterbox'
+};
 
+app.init = function(){
+
+};
+
+app.send = function(message){
+  $.ajax({
+    url: 'https://api.parse.com/1/classes/chatterbox',
+    type: 'POST',
+    data: JSON.stringify(message),
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Message sent');
+    },
+    error: function (data) {
+      console.error('chatterbox: Failed to send message');
+    }
+  });
+};
+
+app.fetch = function(){
+  $.ajax({
+    // always use this url
+    url: 'https://api.parse.com/1/classes/chatterbox',
+    type: 'GET',
+    data: JSON.stringify(message),
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Message sent');
+    },
+    error: function (data) {
+      // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to send message');
+    }
+  });
+};
+
+app.clearMessages = function(){
+  $("#chats").empty();
+};
+
+app.addMessage = function(message){
+  $("#chats").append("<p>" + message.text + "</p>");
+};
+
+app.addRoom = function(string){
+  $("#roomSelect").append("<p>" + string + "</p>");
+};
