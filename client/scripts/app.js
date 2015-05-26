6a04d412dc40e14eed2abf6b2cafba090293ac65
Hack Reactor Students <accounts@hackreactor.com>
2015-05-25 17:31:53 -0700

    Make friends list appear

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -15,6 +15,7 @@ app.send = function(message){
     contentType: 'application/json',
     success: function (data) {
       console.log('chatterbox: Message sent');
+      console.log(data);
     },
     error: function (data) {
       console.error('chatterbox: Failed to send message');
@@ -22,15 +23,19 @@ app.send = function(message){
   });
 };
 
+var userObjects = {};
 app.fetch = function(){
   $.ajax({
     // always use this url
     url: 'https://api.parse.com/1/classes/chatterbox',
     type: 'GET',
-    data: JSON.stringify(message),
+    data: JSON.stringify(),
     contentType: 'application/json',
     success: function (data) {
       console.log('chatterbox: Message sent');
+      console.log(data);
+      // userObjects = data;
+      app.listFriends(data);
     },
     error: function (data) {
       // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
@@ -39,14 +44,48 @@ app.fetch = function(){
   });
 };
 
+app.listFriends = function(userObjects){
+  for(var i=0; i<userObjects.results.length; i++){
+    console.log(userObjects.results[i].username);
+    $("#friendsList").append("<li class='friendLI'>" + userObjects.results[i].username + "</li>");
+  }
+};
+
 app.clearMessages = function(){
-  $("#chats").empty();
+  $('#chats').empty();
 };
 
 app.addMessage = function(message){
-  $("#chats").append("<p>" + message.text + "</p>");
+  $('#chats')
+    .append('<div class="message"><span class = "username">' + message.username +'</span><br><span class="text">' + message.text + '</span><br><span class="room">' + message.roomname + '</span></div>');
 };
 
 app.addRoom = function(string){
   $("#roomSelect").append("<p>" + string + "</p>");
 };
+
+
+app.addFriend = function(){
+
+};
+$("#listFriendsButton").on("click", function(){
+  alert("hi");
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
